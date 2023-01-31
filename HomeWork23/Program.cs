d065f07439acc64494f003786a1cc5ae20b10eca
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-31 23:02:54 +0300

    HomeWork23/

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,50 @@
+﻿Cubing cubing1 = new Cubing(3);
+cubing1.GetResult();
+
+Console.WriteLine();
+
+Cubing cubing2 = new Cubing();
+cubing2.SetNumber();
+cubing2.GetResult();
+
+
+class Cubing
+{
+    double Number { get; set; }
+
+    public Cubing() { }
+    public Cubing(double number)
+    {
+        Number = number;
+    }
+    
+    public void GetResult()
+    {        
+        double i = 1;
+
+        Console.Write($"Таблица кубов чисел от 1 до {Number}: ");
+
+        while (i <= Number)
+        {
+            Console.Write($"{Math.Pow(i, 3)}");            
+            if(i < Number)
+            {
+                Console.Write(", ");
+            }
+            i++;
+        }
+        //Number = number;
+    }
+     public Cubing SetNumber()
+     {
+        Cubing cubing = new Cubing();
+        double number;
+        Console.Write("Введите число: ");
+        while(!double.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Хватит косячить! Введите натуральное число: ");
+        }
+        Number = number;
+        return cubing;
+     }
+}
